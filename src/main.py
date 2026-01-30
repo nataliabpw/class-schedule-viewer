e63cfddd214145e63e0688fb3bf387d93fc05986
nataliabpw <01187514@pw.edu.pl>
2026-01-30 17:34:25 +0100

    Read schedule name from spreadsheet and ask user for date

--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+from datetime import date
 from pathlib import Path
 import pandas as pd
 
@@ -6,7 +7,21 @@ def main():
     data_path = project_root / 'data' / 'example_schedule.xls'
 
     df = pd.read_excel(data_path)
-    print(df.head())
+
+    SCHEDULE_NAME = df.iloc[0,0]
+    print(f"Schedule Name: {SCHEDULE_NAME}")
+
+    date = get_date_from_user()
+    print(f"Date: {date}")
+
+def get_date_from_user():
+    # print("Please enter a date: ")
+    # day = input("Day: ")
+    # month = input("Month: ")
+    # year = input("Year: ")
+    # return date(int(year), int(month), int(day))
+    return date(2025, 11, 3) # Hardcoded for testing
+
 
 if __name__ == "__main__":
     main()
