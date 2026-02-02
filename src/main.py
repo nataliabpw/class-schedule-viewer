d92c60945968425ec30cf0db2a282882f743ce54
nataliabpw <01187514@pw.edu.pl>
2026-02-02 21:17:32 +0100

    Add weekday calculation based on date

--- src/main.py
+++ src/main.py
@@ -2,17 +2,24 @@ from datetime import date
 from pathlib import Path
 import pandas as pd
 
+WEEKDAYS = ['PONIEDZIAŁEK', 'WTOREK', 'ŚRODA', 'CZWARTEK', 'PIĄTEK']
+
 def main():
     project_root = Path(__file__).parent.parent
     data_path = project_root / 'data' / 'example_schedule.xls'
 
     df = pd.read_excel(data_path)
 
-    SCHEDULE_NAME = df.iloc[0,0]
+    SCHEDULE_NAME = df.columns[0]
     print(f"Schedule Name: {SCHEDULE_NAME}")
 
     date = get_date_from_user()
-    print(f"Date: {date}")
+    weekday_id = date.weekday()  # Monday is 0 and Sunday is 6
+    if weekday_id > 4:
+        print("No classes on weekends!")
+        return
+    print(f"Date: {date}\nDay of the week: {WEEKDAYS[weekday_id]}")
+
 
 def get_date_from_user():
     # print("Please enter a date: ")
