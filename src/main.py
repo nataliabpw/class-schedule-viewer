d2da1b605b9cadf20957670046157add78b7d8b2
Natalia Biernacka <153288099+nataliabpw@users.noreply.github.com>
2026-02-06 21:46:31 +0000

    find columns with matching date

--- src/main.py
+++ src/main.py
@@ -1,10 +1,8 @@
-from datetime import date
+from datetime import datetime, date
 from pathlib import Path
 import pandas as pd
 
 WEEKDAYS = ['PONIEDZIAŁEK', 'WTOREK', 'ŚRODA', 'CZWARTEK', 'PIĄTEK']
-
-WEEKDAYS = ['PONIEDZIAŁEK','WTOREK','ŚRODA','CZWARTEK','PIĄTEK']
 
 def main():
     project_root = Path(__file__).parent.parent
@@ -12,7 +10,6 @@ def main():
 
     df = pd.read_excel(data_path)
 
-    SCHEDULE_NAME = df.columns[0]
     SCHEDULE_NAME = df.columns[0]
     print(f"Schedule Name: {SCHEDULE_NAME}")
 
@@ -32,11 +29,42 @@ def main():
     for column_id, cell in enumerate(df.iloc[weekday_row]):
         if str(cell).strip() == WEEKDAYS[weekday_id]:
             weekday_start_column_id = column_id
-        elif is_weekday_start_column_id_set(weekday_start_column_id) and pd.notna(cell):
+        elif is_next_weekday_reached(weekday_start_column_id, cell):
             weekday_end_column_id = column_id - 3
             break
     
     print(f"Columns: {weekday_start_column_id} - {weekday_end_column_id}")
+    
+    date_row = weekday_row + 2
+    matching_date_columns = []
+    # TO-DO dodać obsługę niestandardowych dat jak np.01.paż lub cały semestr
+    # dodać obsługę scalonych komórek jak np. kolumny 2-7
+    for column_id, cell in enumerate(df.iloc[date_row, weekday_start_column_id:weekday_end_column_id+1]):
+        cell = str(cell).strip()
+        DASH = '-'
+        if DASH in cell:
+            dash_index = cell.index(DASH)
+            start_date = cell[0:dash_index].strip()
+            end_date = cell[dash_index+1:].strip()
+
+            # zamiana str na datę
+            start_date = format_date(start_date, date)
+            end_date = format_date(end_date, date)
+            # obsługa stycznia
+            if start_date>end_date:
+                end_date = end_date.replace(year=end_date.year + 1)
+
+            # (sprawdzenie czy zakres pasuje do daty)
+            if start_date <= date <= end_date:
+                # (jeśli tak to zapamiętanie kolumny)
+                matching_date_columns.append(column_id+weekday_start_column_id)
+        elif cell=='cały semestr':
+            matching_date_columns.append(column_id+weekday_start_column_id )
+        elif cell != 'nan':
+            date_from_cell = format_date(cell, date)
+            if date_from_cell==date:
+                matching_date_columns.append(column_id+weekday_start_column_id )
+    print(matching_date_columns)
 
 def get_date_from_user():
     # print("Please enter a date: ")
@@ -44,13 +72,30 @@ def get_date_from_user():
     # month = input("Month: ")
     # year = input("Year: ")
     # return date(int(year), int(month), int(day))
-    return date(2025, 11, 7) # Hardcoded for testing
+    return date(2025, 11, 3) # Hardcoded for testing
+
+def is_next_weekday_reached(weekday_start_column_id, cell):
+    if is_weekday_start_column_id_set(weekday_start_column_id) and pd.notna(cell):
+        return True
+    return False
 
 def is_weekday_start_column_id_set(weekday_start_column_id):
     if weekday_start_column_id==-1:
         return False
     else:
         return True
+
+def format_date(date_to_format, date_for_year):
+    # TO-DO
+    # dodać obsługę komentarza (bez xx.yy)
+    # teraz komentarz jest pomijany
+    if date_to_format[-1]==')':
+        id = date_to_format.index('(')
+        date_to_format = date_to_format[:id].strip()
+    if date_to_format[-1]!='.':
+        date_to_format += '.'
+    date_to_format += str(date_for_year.year)
+    return datetime.strptime(date_to_format, "%d.%m.%Y").date()
 
 if __name__ == "__main__":
     main()
