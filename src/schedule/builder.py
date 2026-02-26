055316c2cbe520e2055fc1062e29db8bb7637ccc
Natalia Biernacka <153288099+nataliabpw@users.noreply.github.com>
2026-02-26 12:50:53 +0000

    Display additional schedule information

--- src/schedule/builder.py
+++ src/schedule/builder.py
@@ -1,6 +1,8 @@
 def build_group_schedule(class_name_row, start_row, end_row, matching_date_columns, df, group_seminaria, group_cwiczenia, group_zajecia):
     classes = [None] * (end_row - start_row)
 
+    class_info_row = class_name_row + 2
+
     for column_id in matching_date_columns:
         if 'ćwiczenia' in df.iloc[class_name_row, column_id].lower():
             group = group_cwiczenia
@@ -15,19 +17,25 @@ def build_group_schedule(class_name_row, start_row, end_row, matching_date_colum
                 if group_index+len(group) < len(cell):
                     if cell[group_index+len(group)] in '0123456789':
                         continue
-                classes[row_id-start_row] = df.iloc[class_name_row, column_id].strip()+" - "+cell
+                classes[row_id-start_row] = {
+                    "name":df.iloc[class_name_row, column_id].strip()+" - "+cell, 
+                    "location": df.iloc[class_info_row, column_id].strip()
+                }
     return classes
 
 def format_schedule_with_time(classes, df, start_row, time_column_id):
     last_class = None
     schedule = []
-    for row_id, curr_class in enumerate(classes):
+    for row_id, curr_class_entry in enumerate(classes):
+
+        curr_class = curr_class_entry["name"] if curr_class_entry is not None else None
 
         if last_class is not None and curr_class!=last_class:
             end_time = df.iloc[row_id+start_row-1, time_column_id]
             end_time = end_time[end_time.index('-')+1:].strip()
             schedule.append({
                 "name": last_class,
+                "info": last_class_info,
                 "start": start_time,
                 "end": end_time
             })
@@ -37,4 +45,5 @@ def format_schedule_with_time(classes, df, start_row, time_column_id):
             start_time = start_time[:start_time.index('-')].strip()
 
         last_class = curr_class
+        last_class_info = curr_class_entry["location"] if curr_class_entry is not None else None
     return schedule
