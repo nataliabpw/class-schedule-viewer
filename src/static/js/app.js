b235fc6ed93d69215bd0b95f7ac56047bc705ba5
Natalia Biernacka <153288099+nataliabpw@users.noreply.github.com>
2026-03-02 15:56:24 +0000

    Implement Bootstrap layout and styling

--- src/static/js/app.js
+++ src/static/js/app.js
@@ -10,7 +10,11 @@
         const button = form.querySelector('button[type="submit"]');
 
         button.disabled = true;
-        document.getElementById('schedule-container').innerHTML = '<p>Ładowanie...</p>';
+        document.getElementById('schedule-container').innerHTML = `
+            <div class="text-center">
+            <div class="spinner-border text-primary" role="status"></div>
+            </div>
+        `;
 
         const params = new URLSearchParams(new FormData(form)).toString();
         try{
@@ -44,30 +48,55 @@
         clearErrors();
         scheduleDiv.innerHTML = '';
 
-        const title = document.createElement('h2');
-        title.textContent = data.schedule_name;
-        scheduleDiv.appendChild(title);
+        const card = document.createElement('div');
+        card.classList.add('card', 'shadow-sm', 'mb-4');
+        scheduleDiv.appendChild(card);
 
-        const subtitle = document.createElement('h3');
-        subtitle.textContent = `${data.selected_date} - ${data.weekday}`;
-        scheduleDiv.appendChild(subtitle);
+        const cardBody = document.createElement('div');
+        cardBody.classList.add('card-body');
+        card.appendChild(cardBody);
+
+        // Header
+        const header = document.createElement('div');
+        header.classList.add('mb-3');
+
+        header.innerHTML = `
+            <div class="text-center">
+                <h5 class="mb-1 fw-semibold">${data.schedule_name}</h5>
+                <div class="text-muted small">
+                    ${data.selected_date} • ${data.weekday}
+                </div>
+            </div>
+        `;
+
+        cardBody.appendChild(header);
 
         if (data.schedule.length === 0) {
             const noClasses = document.createElement('p');
+            noClasses.classList.add('text-muted');
             noClasses.textContent = 'Brak zajęć w tym dniu.';
-            scheduleDiv.appendChild(noClasses);
+            cardBody.appendChild(noClasses);
             return;
         }
 
         // Table
         const table = document.createElement('table');
+        table.classList.add('table', 'table-hover', 'align-middle', 'mt-3', 'text-center');
+        
+        const thead = document.createElement('thead');
+        thead.classList.add('table-light');
+
         const headerRow = document.createElement('tr');
         ['Start', 'Koniec', 'Zajęcia', 'Dodatkowe informacje'].forEach(text => {
             const th = document.createElement('th');
+            th.classList.add('align-middle', 'text-center', 'fw-semibold');
             th.textContent = text;
             headerRow.appendChild(th);
         });
-        table.appendChild(headerRow);
+        thead.appendChild(headerRow);
+        table.appendChild(thead);
+
+        const tbody = document.createElement('tbody');
 
         data.schedule.forEach(entry => {
             const row = document.createElement('tr');
@@ -76,9 +105,10 @@
                 td.textContent = cell;
                 row.appendChild(td);
             })
-            table.appendChild(row);
+            tbody.appendChild(row);
         })
-        scheduleDiv.appendChild(table);
+        table.appendChild(tbody);
+        cardBody.appendChild(table);
     }
 
     form.addEventListener('submit', (event) => {
